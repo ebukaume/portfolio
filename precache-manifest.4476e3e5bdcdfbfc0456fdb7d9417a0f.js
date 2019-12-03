eb0231a665c8ff377118921904d7f03b37ff6095
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-12-03 13:48:52 +0100

    Updates

---
+++ precache-manifest.4476e3e5bdcdfbfc0456fdb7d9417a0f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b558d271fa1a0385a303bc63335840c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "407bfb8557a67b956d2b",
+    "url": "/portfolio/static/css/main.07003a4d.chunk.css"
+  },
+  {
+    "revision": "52c711b7c2696f62a8e5",
+    "url": "/portfolio/static/js/2.548a4dcc.chunk.js"
+  },
+  {
+    "revision": "407bfb8557a67b956d2b",
+    "url": "/portfolio/static/js/main.631b0ebf.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "4032c3afdc0cd20b5963668f7280533d",
+    "url": "/portfolio/static/media/UMEOKONKWO EBUKA'S CV.4032c3af.pdf"
+  },
+  {
+    "revision": "95bda11f89cf96c193d05b3eb1cef3a7",
+    "url": "/portfolio/static/media/bookstore-api.95bda11f.png"
+  },
+  {
+    "revision": "2985f23cd50856cfbe943651569c1b27",
+    "url": "/portfolio/static/media/bookstore.2985f23c.png"
+  },
+  {
+    "revision": "5689c7fd9a751d51ce45c6d47b2f8496",
+    "url": "/portfolio/static/media/facebook.5689c7fd.png"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/portfolio/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "b986b4fd45fd54c447cd5783082c397f",
+    "url": "/portfolio/static/media/gmail.b986b4fd.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/portfolio/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "f9579efae8d81a00bacede010c2f4bd8",
+    "url": "/portfolio/static/media/microverse.f9579efa.jpeg"
+  },
+  {
+    "revision": "ffba1c0838825777c3311a770324fe3b",
+    "url": "/portfolio/static/media/profile_picture_2.ffba1c08.jpeg"
+  },
+  {
+    "revision": "f681cd5f2897114e3d1521439e2e7f9b",
+    "url": "/portfolio/static/media/stackoverflow.f681cd5f.svg"
+  },
+  {
+    "revision": "62f487ef616e9b8c735db0884c3fcaab",
+    "url": "/portfolio/static/media/todolist.62f487ef.png"
+  },
+  {
+    "revision": "f7cfa42485c3597876a955aa1dd3048c",
+    "url": "/portfolio/static/media/top.f7cfa424.svg"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/portfolio/static/media/twitter.be066898.svg"
+  }
+]);
